3de1d7f34a01f8022ccda313554ed62429d8a4d3
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-11-23 16:10:23 +0900

    Add :: Padding Widget Extension

--- lib/component/extension/widget.dart
+++ lib/component/extension/widget.dart
@@ -1,9 +1,13 @@
 
 import 'package:flutter/material.dart';
 
+bool isDebug = true;
+
 extension WidgetExtension on Widget {
   Widget redBoard() {
+    if (!isDebug) return this;
     return Container(
+      padding: const EdgeInsets.only(top: 1, left: 1),
       decoration: BoxDecoration(
         border: Border.all(
           color: Colors.blue,
@@ -13,4 +17,24 @@ extension WidgetExtension on Widget {
       child: this,
     );
   }
+
+  Widget paddingOnly({
+    double top = 0,
+    double left = 0,
+    double right = 0,
+    double bottom = 0}) {
+    return Container(
+      padding: EdgeInsets.only(top: top, left: left, right: right, bottom: bottom),
+      child: this,
+    );
+  }
+
+  Widget paddingAll(double padding) {
+    return Container(
+      padding: EdgeInsets.all(padding),
+      child: this,
+    );
+  }
 }
+
+
