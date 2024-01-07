8fa0cc02bf2c6a99265b139e16ef0e893dcae477
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-08 00:03:28 +0900

    feat : add font ext

---
+++ Projects/DesignSystem/Sources/Theme/FontExt.swift
@@ -0,0 +1,28 @@
+//
+//  FontExt.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 1/8/24.
+//  Copyright © 2024 hhhello0507. All rights reserved.
+//
+
+import SwiftUI
+
+
+extension Font {
+    
+    // pretendard
+    static let title = Font.custom(Pretendard.bold.rawValue, size: 24)
+    static let subtitle = Font.custom(Pretendard.bold.rawValue, size: 20)
+    static let body = Font.custom(Pretendard.bold.rawValue, size: 16)
+    static let bodyLight = Font.custom(Pretendard.medium.rawValue, size: 16)
+    static let label = Font.custom(Pretendard.medium.rawValue, size: 14)
+    static let caption = Font.custom(Pretendard.medium.rawValue, size: 12)
+    static let cute = Font.custom(Pretendard.medium.rawValue, size: 10)
+}
+
+enum Pretendard: String {
+    
+    case bold = "Pretendard-Bold"
+    case medium = "Pretendard-Medium"
+}
