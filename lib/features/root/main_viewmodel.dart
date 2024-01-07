b82add0b7449b057a4237024785db409119103a3
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-07 23:10:31 +0900

    remove : restart project

--- lib/features/root/main_viewmodel.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-class MainViewModel extends ChangeNotifier {
-
-  final String tt = "tt";
-}
