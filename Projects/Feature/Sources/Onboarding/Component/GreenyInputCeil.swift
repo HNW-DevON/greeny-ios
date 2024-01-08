57a633db3abd97b868df65ac877eddb1da602f36
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-08 15:31:15 +0900

    feat : add input ceil component

---
+++ Projects/Feature/Sources/Onboarding/Component/GreenyInputCeil.swift
@@ -0,0 +1,41 @@
+//
+//  InputCeil.swift
+//  Feature
+//
+//  Created by dgsw8th71 on 1/8/24.
+//  Copyright © 2024 hhhello0507. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct GreenyInputCeil: View {
+    
+    var textFieldType: GreenyTextFieldType
+    var title: String
+    var hint: String
+    @Binding var text: String
+    
+    init(textFieldType: GreenyTextFieldType = .none(hasXMark: true),
+         title: String,
+         hint: String,
+         text: Binding<String>) {
+        self.textFieldType = textFieldType
+        self.title = title
+        self.hint = hint
+        self._text = text
+    }
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            HStack {
+                Text(title)
+                    .font(._subtitle)
+                    .padding(.leading, 4)
+                Spacer()
+            }
+            GreenyTextField("아이디를 입력해 주세요", text: $text)
+        }
+        .padding(.horizontal, 20)
+    }
+}
