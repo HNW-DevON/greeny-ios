7964a4a5b84724965a3d3313379aa2a95058ed70
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-09 11:19:32 +0900

    feat : add top bar in onboarding views

--- Projects/Feature/Sources/Onboarding/Join/JoinSecondView.swift
+++ Projects/Feature/Sources/Onboarding/Join/JoinSecondView.swift
@@ -13,30 +13,35 @@ struct JoinSecondView: View {
     
     @State var name: String = ""
     @State var birthday: String = ""
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack(spacing:0) {
-            GreenyInputCeil(title: "이름", hint: "이름을 입력해 주세요", text: $name)
-                .padding([.bottom, .top], 32)
-            GreenyInputCeil(textFieldType: .password, title: "생년월일", hint: "생년월일을 입력해 주세요", text: $birthday)
-            Spacer()
-            
-            HStack(spacing: 0) {
-                Text("이미 계정이 있으시다면? ")
-                    .font(._caption)
-                NavigationLink {
-                    LoginView()
-                } label: {
-                    Text("로그인")
-                        .foregroundStyle(Color.main600)
+        GreenyTopbar("회원가입") {
+            dismiss()
+        } content: {
+            VStack(spacing:0) {
+                GreenyInputCeil(title: "이름", hint: "이름을 입력해 주세요", text: $name)
+                    .padding([.bottom, .top], 32)
+                GreenyInputCeil(textFieldType: .password, title: "생년월일", hint: "생년월일을 입력해 주세요", text: $birthday)
+                Spacer()
+                
+                HStack(spacing: 0) {
+                    Text("이미 계정이 있으시다면? ")
                         .font(._caption)
+                    NavigationLink {
+                        LoginView()
+                    } label: {
+                        Text("로그인")
+                            .foregroundStyle(Color.main600)
+                            .font(._caption)
+                    }
                 }
-            }
-            .padding(.bottom, 16)
-            
-            GreenyButton("회원가입") {}
                 .padding(.bottom, 16)
-            
+                
+                GreenyButton("회원가입") {}
+                    .padding(.bottom, 16)
+                
+            }
         }
         .navigationBarBackButtonHidden()
     }
--- Projects/Feature/Sources/Onboarding/Login/LoginView.swift
+++ Projects/Feature/Sources/Onboarding/Login/LoginView.swift
@@ -13,45 +13,51 @@ struct LoginView: View {
     
     @State var id: String = ""
     @State var pw: String = ""
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack(spacing:0) {
-            GreenyInputCeil(title: "아이디", hint: "아이디를 입력해 주세요", text: $id)
-                .padding([.bottom, .top], 32)
-            GreenyInputCeil(textFieldType: .password, title: "비밀번호", hint: "비밀번호를 입력해 주세요", text: $pw)
-            HStack {
-                Spacer()
-                NavigationLink {
+        GreenyTopbar("로그인") {
+            dismiss()
+        } content: {
+            VStack(spacing:0) {
+                GreenyInputCeil(title: "아이디", hint: "아이디를 입력해 주세요", text: $id)
+                    .padding([.bottom, .top], 32)
+                GreenyInputCeil(textFieldType: .password, title: "비밀번호", hint: "비밀번호를 입력해 주세요", text: $pw)
+                HStack {
+                    Spacer()
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("비밀번호 찾기")
+                            .foregroundStyle(Color.gray500)
+                            .font(._caption)
+                            .underline()
+                            .padding([.top], 4)
+                            .padding([.trailing], 24)
+                    }
                     
-                } label: {
-                    Text("비밀번호 찾기")
-                        .foregroundStyle(Color.gray500)
-                        .font(._caption)
-                        .underline()
-                        .padding([.top], 4)
-                        .padding([.trailing], 24)
                 }
+                Spacer()
                 
-            }
-            Spacer()
-            
-            HStack(spacing: 0) {
-                Text("아직 계정이 없으시다면? ")
-                    .font(._caption)
-                NavigationLink {
-                    JoinFirstView()
-                } label: {
-                    Text("회원가입")
-                        .foregroundStyle(Color.main600)
+                HStack(spacing: 0) {
+                    Text("아직 계정이 없으시다면? ")
                         .font(._caption)
+                    NavigationLink {
+                        JoinFirstView()
+                    } label: {
+                        Text("회원가입")
+                            .foregroundStyle(Color.main600)
+                            .font(._caption)
+                    }
                 }
-            }
-            .padding(.bottom, 16)
-            
-            
-            GreenyButton("로그인") {}
                 .padding(.bottom, 16)
+                
+                
+                GreenyButton("로그인") {}
+                    .padding(.bottom, 16)
+            }
         }
+        
         .navigationBarBackButtonHidden()
     }
 }
