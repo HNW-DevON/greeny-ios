88871f3954592c1d00265b28ecdfe473901b56df
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-23 22:28:10 +0900

    feat : add quest ceil in home view

---
+++ Projects/Feature/Sources/Main/Home/Component/QuestCeil.swift
@@ -0,0 +1,36 @@
+//
+//  QuestCeil.swift
+//  Feature
+//
+//  Created by dgsw8th71 on 1/23/24.
+//  Copyright © 2024 hhhello0507. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct QuestCeil: View {
+    
+    var image: String
+    var title: String
+    
+    var body: some View {
+        
+        let roundedCorner = RoundedCorner(radius: Size.extraLarge.rawValue, corners: .allCorners)
+        
+        HStack(spacing: 12) {
+            Image(image)
+            Text(title)
+                .font(._body)
+        }
+        .frame(maxHeight: 100)
+        .padding(24)
+        .background(Color.gray100)
+        .clipShape(roundedCorner)
+        .overlay {
+            roundedCorner
+                .stroke(Color.gray300, lineWidth: 2)
+        }
+        .padding(.vertical, 1)
+    }
+}
--- Projects/Feature/Sources/Main/Home/HomeView.swift
+++ Projects/Feature/Sources/Main/Home/HomeView.swift
@@ -16,10 +16,15 @@ struct Today: Hashable {
     let createdAt: String
 }
 
-fileprivate let dummyTodayGreenyData = [
+fileprivate let dummyTodayData = [
     Today(imageUrl: "https://hws.dev/paul.jpg", productName: "착한치킨", author: "이슬아", createdAt: "2시간 전"),
     Today(imageUrl: "https://hws.dev/paul.jpg", productName: "착한치킨", author: "이슬아", createdAt: "2시간 전"),
     Today(imageUrl: "https://hws.dev/paul.jpg", productName: "착한치킨", author: "이슬아", createdAt: "2시간 전"),
+]
+
+fileprivate let dummyQuestData = [
+    ("Today", "착하게\n시작하는 하루"),
+    ("New", "새 퀘스트")
 ]
 
 public struct HomeView: View {
@@ -32,18 +37,27 @@ public struct HomeView: View {
                 VStack(spacing: 0) {
                     GreenyTitle("오늘의 착한 소비")
                         .padding(.top, 20)
-                    ScrollView(.horizontal) {
+                    ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 8) {
-                            ForEach(dummyTodayGreenyData, id: \.self) {
+                            ForEach(dummyTodayData, id: \.self) {
                                 TodayCeil(todayGreeny: $0)
                             }
                         }
-                        .padding(.top, 8)
                         .padding(.leading, 16)
                     }
+                    .padding(.top, 8)
                     GreenyTitle("카테고리")
                         .padding(.top, 36)
                     GreenyTitle("퀘스트")
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 16) {
+                            ForEach(dummyQuestData, id: \.0) {
+                                QuestCeil(image: $0.0, title: $0.1)
+                            }
+                        }
+                        .padding(.leading, 16)
+                    }
+                    .padding(.top, 8)
                 }
             }
         }
