05a70bf4024166c53486465907ae5835b8b3240b
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-31 21:50:18 +0900

    feat : add greeny tag component

---
+++ Projects/DesignSystem/Sources/Component/Tag/GreenyTag.swift
@@ -0,0 +1,33 @@
+//
+//  GreenyTag.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 1/31/24.
+//  Copyright © 2024 hhhello0507. All rights reserved.
+//
+
+import SwiftUI
+
+struct GreenyTag: View {
+    
+    let text: String
+    let backgroundColor: Color
+    let foregroundColor: Color
+    
+    init(_ text: String,
+         foregroundColor: Color,
+         backgroundColor: Color
+    ) {
+        self.text = text
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+    }
+    
+    var body: some View {
+        Text(text)
+            .padding(.vertical, 5)
+            .padding(.horizontal, 8)
+            .foregroundStyle(foregroundColor)
+            .background(backgroundColor)
+    }
+}
