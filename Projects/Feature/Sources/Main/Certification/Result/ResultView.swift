5c405d5234fded67c65b3b05a563142ed9ee0e99
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-04 09:35:33 +0900

    feat : create result view content

--- Projects/Feature/Sources/Main/Certification/Result/ResultView.swift
+++ Projects/Feature/Sources/Main/Certification/Result/ResultView.swift
@@ -14,6 +14,97 @@ struct ResultView: View {
     @Environment(\.dismiss) var dismiss
     @ObservedObject var vm = ResultViewModel()
     
+    
+    @ViewBuilder
+    private var product: some View {
+        HStack(alignment: .top, spacing: 16) {
+            AsyncImage(url: URL(string: "https://hws.dev/paul.jpg"),
+                       content: {
+                $0.image?
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .addGrayStroke()
+            }
+            )
+            VStack(alignment: .leading, spacing: 0) {
+                Text("망고맛치킨")
+                    .font(._body)
+                Text("히히 망고. 망고는 맛있어. 맛있으면\n망고")
+                    .font(._caption)
+                    .padding(.top, 4)
+                HStack(spacing: 0) {
+                    Text("312")
+                        .foregroundStyle(Color.main600)
+                        .font(._cute)
+                    Text("번 인증된 제품이에요.")
+                        .foregroundStyle(Color.gray500)
+                        .font(._cute)
+                }
+                .padding(6)
+                .background(Color.gray100)
+                .clipShape(RoundedCorner(radius: 8))
+                .padding(.top, 12)
+            }
+            .padding(.top, 4)
+            .toLeading()
+        }
+    }
+    
+    @ViewBuilder
+    private var company: some View {
+        HStack(alignment: .top, spacing: 16) {
+            AsyncImage(url: URL(string: "https://hws.dev/paul.jpg"),
+                       content: {
+                $0.image?
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .addGrayStroke()
+            }
+            )
+            VStack(alignment: .leading, spacing: 0) {
+                Text("제갈치킨")
+                    .font(._body)
+                Text("선캄브리아 시대부터 춘추제갈\n시대를 지나 현대 문명에 큰 기축을\n담당한 사회적 기업의 근본 중의\n근본 치킨 매장이다.")
+                    .font(._caption)
+                    .padding(.top, 4)
+                HStack(spacing: 0) {
+                    Text("환경")
+                        .foregroundStyle(Color(0x47AF23))
+                        .font(._cute)
+                }
+                .padding(6)
+                .background(Color(0xCBFFCD))
+                .clipShape(RoundedCorner(radius: 8))
+                .padding(.top, 12)
+            }
+            .padding(.top, 4)
+            .toLeading()
+        }
+    }
+    
+    @ViewBuilder
+    private var content: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                Text("분석된 결과는...")
+                    .font(._title)
+                    .toLeading()
+                    .padding(.leading, 20)
+                product
+                    .padding(.horizontal, 16)
+                    .padding(.top, 8)
+                Text("기업 정보")
+                    .font(._title)
+                    .toLeading()
+                    .padding(.leading, 20)
+                    .padding(.top, 24)
+                company
+                    .padding(.horizontal, 16)
+                    .padding(.top, 8)
+            }
+        }
+    }
+    
     var body: some View {
         GreenyTopbar("인증", backButtonCallback: vm.viewType == .result ? {
             dismiss()
@@ -25,7 +116,7 @@ struct ResultView: View {
                 case .loadingSecond:
                     LoadingSecondView()
                 case .result:
-                    Text("result")
+                    content
                 }
             }
         }
