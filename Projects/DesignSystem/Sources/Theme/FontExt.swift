1a23601cdb1a4cd95d5a3b4b5810661f425a7298
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-05 22:21:57 +0900

    fix : custome font to tuist font

--- Projects/DesignSystem/Sources/Theme/FontExt.swift
+++ Projects/DesignSystem/Sources/Theme/FontExt.swift
@@ -11,16 +11,12 @@ import SwiftUI
 
 public extension Font {
     // pretendard
-    static let _title = Font.custom(Pretendard.bold.rawValue, size: 24)
-    static let _subtitle = Font.custom(Pretendard.bold.rawValue, size: 20)
-    static let _body = Font.custom(Pretendard.bold.rawValue, size: 16)
-    static let _bodyLight = Font.custom(Pretendard.medium.rawValue, size: 16)
-    static let _label = Font.custom(Pretendard.medium.rawValue, size: 14)
-    static let _caption = Font.custom(Pretendard.medium.rawValue, size: 12)
-    static let _cute = Font.custom(Pretendard.medium.rawValue, size: 10)
+    static let _title = Font(DesignSystemFontFamily.Pretendard.bold.font(size: 24))
+    static let _subtitle = Font(DesignSystemFontFamily.Pretendard.bold.font(size: 20))
+    static let _body = Font(DesignSystemFontFamily.Pretendard.bold.font(size: 16))
+    static let _bodyLight = Font(DesignSystemFontFamily.Pretendard.medium.font(size: 16))
+    static let _label = Font(DesignSystemFontFamily.Pretendard.medium.font(size: 14))
+    static let _caption = Font(DesignSystemFontFamily.Pretendard.medium.font(size: 12))
+    static let _cute = Font(DesignSystemFontFamily.Pretendard.medium.font(size: 10))
 }
 
-enum Pretendard: String {
-    case bold = "Pretendard-Bold"
-    case medium = "Pretendard-Medium"
-}
