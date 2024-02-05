692c2a6045d8b58b15cd7e9a55c214e4c12b6e07
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-05 21:44:35 +0900

    feat : init point api

---
+++ Projects/Service/Sources/Api/PointApi.swift
@@ -0,0 +1,21 @@
+//
+//  PointApi.swift
+//  Service
+//
+//  Created by dgsw8th71 on 2/5/24.
+//  Copyright © 2024 hhhello0507. All rights reserved.
+//
+
+import Alamofire
+
+final class PointApi {
+    
+    func savePoint(id: Int) {
+        
+    }
+    
+}
+
+extension PointApi {
+    static let live = PointApi()
+}
