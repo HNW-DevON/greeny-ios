2309ad027924eb3b882f8233fb4f27e20f9f0d6b
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-09 18:18:19 +0900

    chore : add camera permission

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -23,7 +23,9 @@ let project = Project.makeModule(
         "ENABLE_TESTS": .boolean(true),
         "NSAppTransportSecurity": [
             "NSAllowsArbitraryLoads": .boolean(true)
-        ]
+        ],
+        "NSCameraUsageDescription": "이 앱은 사진을 촬영하기 위해 카메라를 사용합니다. 카메라 접근을 허용해 주세요",
+        "NSPhotoLibraryAddUsageDescription": "이 앱은 사진을 촬영 후 저장합니다. 앨범 쓰기 권한을 허용해 주세요."
     ])
 )
 
