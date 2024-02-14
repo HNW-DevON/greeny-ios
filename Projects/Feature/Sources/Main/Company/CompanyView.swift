4f08067f0ec7bf3089082ad941ee6a35ef64a13f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-14 16:38:09 +0900

    fix : company detail ceil style

--- Projects/Feature/Sources/Main/Company/CompanyView.swift
+++ Projects/Feature/Sources/Main/Company/CompanyView.swift
@@ -13,30 +13,49 @@ import Service
 struct CompanyView: View {
     
     var company: Company
+    @Environment(\.dismiss) var dismiss
 
     var body: some View {
         GreenyTopbar("회사 정보") {
+            dismiss()
+        } content:  {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    HStack {
-                        Text("제갈 치킨")
-                            .font(._subtitle)
-                            .padding(.top, 28)
-                        Spacer()
-                        
-                    }
-                    Text("qwdad")
-                    Text("회삼ㅇ 에서 진행하는 이벤트")
-                    HStack {
-                        LazyVStack {
-                            
-                        }
-                        LazyVStack {
-                            
+                    HStack(spacing: 8) {
+                        ForEach(company.companyCategory, id: \.self) {
+                            GreenyTag($0, foregroundColor: .gray, backgroundColor: .red)
                         }
                     }
+                    .padding(.top, 28)
+                    .padding(.leading, 4)
+                    Text(company.companyName)
+                        .font(._title)
+                        .toLeading()
+                        .padding(.top, 8)
+                        .padding(.leading, 4)
+                    Text(company.companyDesc)
+                        .foregroundStyle(Color.gray500)
+                        .font(._bodyLight)
+                        .padding(.top, 4)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 4)
+                        .toLeading()
+                    Divider()
+                        .padding(.top, 16)
+                    Text("\(company.companyName)에서 진행하는 이벤트")
+                        .font(._subtitle)
+                        .toLeading()
+                        .padding(.top, 16)
+                    LazyVStack(spacing: 8) {
+                        ForEach(company.event, id: \.id) { event in
+                            EventView(event: event)
+                        }
+                    }
+                    .padding(.top, 12)
                 }
+                .padding(.horizontal, 20)
             }
         }
+        .navigationBarBackButtonHidden()
     }
 }
--- Projects/Feature/Sources/Main/CompanyDetail/CompanyDetailCeil.swift
+++ Projects/Feature/Sources/Main/CompanyDetail/CompanyDetailCeil.swift
@@ -19,21 +19,23 @@ struct CompanyDetailCeil: View {
             CompanyView(company: company)
         } label: {
             VStack(alignment: .leading, spacing: 0) {
-                Text(company.companyName)
-                    .foregroundStyle(Color.black)
-                    .font(._subtitle)
-                    .padding(.leading, 4)
-                Text(company.companyDesc)
-                    .foregroundStyle(Color.gray500)
-                    .font(._label)
-                    .multilineTextAlignment(.leading)
-                    .padding(.top, 4)
                 HStack {
                     ForEach(company.companyCategory, id: \.self) { category in
                         GreenyTag(category, foregroundColor: Color.green500, backgroundColor: Color.green)
                     }
                 }
-                .padding(.top, 4)
+                .padding(.leading, 4)
+                Text(company.companyName)
+                    .foregroundStyle(Color.black)
+                    .font(._subtitle)
+                    .padding(.leading, 4)
+                    .padding(.top, 8)
+                Text(company.companyDesc)
+                    .foregroundStyle(Color.gray500)
+                    .font(._label)
+                    .multilineTextAlignment(.leading)
+                    .padding(.top, 4)
+                
             }
             .padding(.horizontal, 20)
             .padding(.top, 12)
