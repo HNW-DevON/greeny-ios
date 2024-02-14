f7f99d51847138ab9ae799c7e676cfcf0f0f486a
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-14 14:46:44 +0900

    feat : create category ceil

---
+++ Projects/Feature/Sources/Main/Home/CategoryCeil.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct CategoryCeil: View {
+    
+    var name: String
+    var idx: Int
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack(spacing: 8) {
+                Spacer()
+                Image("Category\(idx + 1)")
+                Text(name)
+                    .foregroundStyle(Color.gray500)
+                    .font(._caption)
+                Spacer()
+            }
+            Spacer()
+        }
+        .background(Color.gray100)
+        .addGrayStroke(color: .gray300)
+        .padding(1)
+    }
+}
