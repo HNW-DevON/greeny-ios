27392154dd8f470896ce5c95117ced1246b53d88
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-14 14:47:03 +0900

    feat : create categories in home view

--- Projects/Feature/Sources/Main/Home/HomeView.swift
+++ Projects/Feature/Sources/Main/Home/HomeView.swift
@@ -14,10 +14,30 @@ let dummyQuestData = [(Asset.today, "착하게\n시작하는 하루", 0),
                       (Asset.new, "새 퀘스트", 1),
                       (Asset.calendarWeek, "이번주의 퀘스트", 2)]
 
+let categories = [
+    "환경","보육","고용","관광운동",
+    "교육","문화재","보건","기타"
+]
 struct HomeView: View {
     
     @ObservedObject var vm = HomeViewModel()
     var questCallback: (Int) -> Void
+    
+    @ViewBuilder
+    private var category: some View {
+        VStack(spacing: 8) {
+            HStack(spacing: 8) {
+                ForEach(Array((Array(categories[0..<4]).enumerated())), id: \.0) { i in
+                    CategoryCeil(name: i.1, idx: i.0)
+                }
+            }
+            HStack(spacing: 8) {
+                ForEach(Array((Array(categories[4..<8]).enumerated())), id: \.0) { i in
+                    CategoryCeil(name: i.1, idx: i.0)
+                }
+            }
+        }
+    }
     
     var body: some View {
         GreenyTopbar("홈") {
@@ -35,7 +55,11 @@ struct HomeView: View {
                     .padding(.top, 8)
                     GreenyTitle("카테고리")
                         .padding(.top, 36)
+                    category
+                        .padding(.top, 8)
+                        .padding(.horizontal, 10)
                     GreenyTitle("퀘스트")
+                        .padding(.top, 16)
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 16) {
                             ForEach(dummyQuestData, id: \.0) { i in
