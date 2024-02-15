e3bde89ee99e06925ebbf48e40b48292ea66df52
bswlkh0310 <hhhello0507@gmail.com>
2024-02-15 22:16:21 +0900

    fix : today company ceil

--- Projects/Feature/Sources/Main/My/Component/TodayCompanyCeil.swift
+++ Projects/Feature/Sources/Main/My/Component/TodayCompanyCeil.swift
@@ -15,34 +15,42 @@ struct TodayCompanyCeil: View {
     var company: Company
     
     var body: some View {
-        HStack {
-//            AsyncImage(
-//                url: URL(string: company.),
-//                content: {
-//                    $0.image?
-//                        .resizable()
-//                        .frame(width: 64, height: 64)
-//                        .addGrayStroke()
-//                }
-//            )
-            VStack {
+        VStack {
+            HStack {
+                //            AsyncImage(
+                //                url: URL(string: company.),
+                //                content: {
+                //                    $0.image?
+                //                        .resizable()
+                //                        .frame(width: 64, height: 64)
+                //                        .addGrayStroke()
+                //                }
+                //            )
+                VStack {
+                    Spacer()
+                    Text(company.companyName)
+                        .font(._body)
+                        .toLeading()
+                        .padding(.leading, 4)
+                    Spacer()
+                    Text(company.companyDesc)
+                        .font(._label)
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                        .toLeading()
+                    Spacer()
+                }
                 Spacer()
-                Text(company.companyName)
-                    .font(._body)
-                    .toLeading()
-                Spacer()
-                Text(company.companyDesc)
-                    .font(._label)
-                    .toLeading()
-                Spacer()
+                
             }
-            .padding(.leading, 12)
-            Spacer()
-            ForEach(company.companyCategory, id: \.self) {
-                let category = Category.fromString($0)
-                GreenyTag($0,
-                          foregroundColor: category.foregroundColor,
-                          backgroundColor: category.backgroundColor)
+            HStack {
+                ForEach(company.companyCategory, id: \.self) {
+                    let category = Category.fromString($0)
+                    GreenyTag($0,
+                              foregroundColor: category.foregroundColor,
+                              backgroundColor: category.backgroundColor)
+                }
+                Spacer()
             }
         }
     }
